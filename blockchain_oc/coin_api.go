9342e23b5dd98b6f5b19e969d21fc88f5a0f6fbf
Rain Gu <rainer.1993@hotmail.com>
2017-07-20 11:48:07 -0700

    still seting up structure

---
+++ blockchain_oc/coin_api.go
@@ -0,0 +1,17 @@
+package blockchain_oc
+
+import(
+	"fmt"
+)
+
+func Register_ID() {
+	fmt.Println("ID Registering.")
+}
+
+func Send_Msg() {
+
+}
+
+func Pick_Msg() {
+	
+}
--- oc/oc.go
+++ oc/oc.go
@@ -8,10 +8,25 @@ import(
 	"fmt"
 	chain "github.com/rainer37/OnionCoin/blockchain_oc"
 	"github.com/rainer37/OnionCoin/crypto_oc/onion"
+	"github.com/rainer37/OnionCoin/peernet"
+	"github.com/rainer37/OnionCoin/crypto_oc/pkpair"
 )
 
 func main() {
-	fmt.Println("OnionCoin Client v1.0.0 Started...")
+	fmt.Println("Onicrypto_oc/pkpairClient v1.0.0 Started...")
 	chain.New_Chain()
 	onion.Onion()
+	peernet.Peernet_Init()
+	peernet.Serve()
+
+	pub, prv := pkpair.KeyGen()
+	
+	cipher := pkpair.Encrypt("Bring them down", pub)
+	plain := pkpair.Decrypt(cipher, prv)
+
+	fmt.Println(plain)
+
+	for {
+
+	}
 }
